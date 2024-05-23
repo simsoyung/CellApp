471ac935ff05bccb07db653e72ba7213bafd7260
soyung sim <simsy1024@gmail.com>
2024-05-23 16:11:03 +0900

    Initial Commit

---
+++ CellApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CellApp
+//
+//  Created by 심소영 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
